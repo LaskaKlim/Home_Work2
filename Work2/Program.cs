17f9e32f718e925ff75b054ae2e981ffcd87b03c
Laska <klimovskih.laska@mail.ru>
2022-12-05 00:54:49 +0700

    семинар 2 задача 1

---
+++ Work2/Program.cs
@@ -0,0 +1,17 @@
+﻿// Вывод случайного трехзначного числа и удаление второй цифры этого числа
+
+Random random = new Random();
+
+int number = 0;
+
+number = random.Next(100, 1000);
+
+Console.WriteLine("сгенерировано " + number);
+
+int a =  number / 100;
+int b = number % 10;
+
+// int text1 = a;
+// int text2 = b; возможное присвоение 
+
+Console.WriteLine($"После удаления второй цифры рультат " + a + b);//a b замен. на text1 text2
