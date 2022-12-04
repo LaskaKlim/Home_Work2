18692e21671dcdb59dd6de8cd249ce029228c462
Laska <klimovskih.laska@mail.ru>
2022-12-05 01:55:49 +0700

    семинар 2 задача 2

--- HM_2/Program.cs
+++ HM_2/Program.cs
@@ -1,8 +1,6 @@
 ﻿Console.WriteLine("Введите трехзначное число: ");
 
 string number = Console.ReadLine();
-int index = 0;
-int n = number[index];
 
 if (number.Length == 3) Console.WriteLine("Третья цифра этого числа : " + number[2]);
 if (number.Length < 3) Console.WriteLine("Третьей цифры не существует");
