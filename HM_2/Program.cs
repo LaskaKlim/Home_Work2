b140bd03293ae42d91c77e560bf9442e35786ed8
Laska <klimovskih.laska@mail.ru>
2022-12-05 10:31:16 +0700

    семинар 2 задача4

--- HM_2/Program.cs
+++ HM_2/Program.cs
@@ -1,7 +1,6 @@
-﻿Console.WriteLine("Введите трехзначное число: ");
+﻿Console.WriteLine("Введите число: ");
 
 string number = Console.ReadLine();
 
-if (number.Length == 3) Console.WriteLine("Третья цифра этого числа : " + number[2]);
+if (number.Length >= 3) Console.WriteLine("Третья цифра этого числа : " + number[2]);
 if (number.Length < 3) Console.WriteLine("Третьей цифры не существует");
-if (number.Length > 3) Console.WriteLine("Введенное чbсло не корректно");
---
+++ HM_3/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Введите порядковый номер дня недели: ");
+
+int number =Convert.ToInt32(Console.ReadLine());
+
+if (number == 6) Console.WriteLine("Да!");
+if (number == 7) Console.WriteLine("Да!");
+if (number < 6) Console.WriteLine("Нет!");
+if (number > 7) Console.WriteLine("Некорректное число");
