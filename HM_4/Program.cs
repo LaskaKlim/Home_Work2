286b0373096e6825b9cff13016e0ff6d6776a73f
Laska <klimovskih.laska@mail.ru>
2022-12-05 10:59:45 +0700

    дз 2 на проверку

--- HM_4/Program.cs
+++ HM_4/Program.cs
@@ -2,7 +2,5 @@
 int a =Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите второе число: ");
 int b =Convert.ToInt32(Console.ReadLine());
-
-if(a == b*b) Console.WriteLine ("Да");
-else if(b == a*a) Console.WriteLine ("Да");
+if(a == b*b | b == a*a) Console.WriteLine ("Да");
 else Console.WriteLine ("Нет");
